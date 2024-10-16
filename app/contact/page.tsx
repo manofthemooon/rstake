8f15de83ea0307045fc8937d16eb3ce90421e8b5
web3ali3n <thirdsaintmob@gmail.com>
2024-10-16 06:49:54 +0300

    v1

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -95,13 +95,13 @@ export default function Example() {
     <div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
       <Navigation />
       <div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-        <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-20 sm:grid-cols-2 lg:grid-cols-3 lg:gap-16">
+        <div className="grid w-full grid-cols-1 gap-4 mx-auto mt-20 sm:grid-cols-2 lg:grid-cols-3 lg:gap-8">
           {socials.map((s) => (
-            <Card key={s.label}>
+            <Card key={s.label} className="h-40"> 
               <Link
                 href={s.href}
                 target="_blank"
-                className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24 lg:pb-48 md:p-16"
+                className="p-4 relative flex flex-col items-center gap-2 duration-700 group md:gap-4 md:py-10 lg:pb-20 md:p-8" // Уменьшение отступов
               >
                 <span
                   className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
@@ -111,10 +111,10 @@ export default function Example() {
                   {s.icon}
                 </span>
                 <div className="z-10 flex flex-col items-center">
-                  <span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
+                  <span className="lg:text-lg font-medium duration-150 xl:text-2xl text-zinc-200 group-hover:text-white font-display">
                     {s.handle}
                   </span>
-                  <span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
+                  <span className="mt-2 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
                     {s.label}
                   </span>
                 </div>
