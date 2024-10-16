29eda4860e079fde9aafcafad4cb0d22755205b3
web3ali3n <thirdsaintmob@gmail.com>
2024-10-16 06:20:15 +0300

    v1

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,49 +3,81 @@ import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
-// Ссылки на иконки из Icons8
-const iconUrls = {
-  twitter: "https://img.icons8.com/ios-filled/50/000000/twitter--v1.png",
-  mail: "https://img.icons8.com/ios-filled/50/000000/email.png",
-  discord: "https://img.icons8.com/ios-filled/50/000000/discord-logo.png",
-  medium: "https://img.icons8.com/ios-filled/50/000000/medium-monogram.png",
-  github: "https://img.icons8.com/ios-filled/50/000000/github.png",
-  telegram: "https://img.icons8.com/ios-filled/50/000000/telegram-app.png",
-};
-
 const socials = [
   {
-    icon: iconUrls.twitter,
-    href: "http://x.com/manoofthemooon",
+    icon: (
+      <img
+        src="https://img.icons8.com/ios-filled/20/ffffff/twitter--v1.png"
+        alt="Twitter"
+        width={20}
+        height={20}
+      />
+    ),
+    href: "https://x.com/manoofthemooon",
     label: "Twitter",
     handle: "@manoofthemooon",
   },
   {
-    icon: iconUrls.telegram,
-    href: "https://t.me/manofthemoon",
-    label: "Telegram",
-    handle: "@manofthemoon",
-  },
-  {
-    icon: iconUrls.github,
-    href: "https://github.com/manofthemooon/",
-    label: "GitHub",
-    handle: "manofthemooon",
-  },
-  {
-    icon: iconUrls.mail,
+    icon: (
+      <img
+        src="https://img.icons8.com/ios-filled/20/ffffff/email.png"
+        alt="Mail"
+        width={20}
+        height={20}
+      />
+    ),
     href: "mailto:manoofthemoonwork@gmail.com",
     label: "Email",
     handle: "manoofthemoonwork@gmail.com",
   },
   {
-    icon: iconUrls.medium,
+    icon: (
+      <img
+        src="https://img.icons8.com/ios-filled/20/ffffff/github.png"
+        alt="GitHub"
+        width={20}
+        height={20}
+      />
+    ),
+    href: "https://github.com/manofthemooon/",
+    label: "GitHub",
+    handle: "manofthemooon",
+  },
+  {
+    icon: (
+      <img
+        src="https://img.icons8.com/ios-filled/20/ffffff/telegram-app.png"
+        alt="Telegram"
+        width={20}
+        height={20}
+      />
+    ),
+    href: "https://t.me/manofthemoon",
+    label: "Telegram",
+    handle: "@manofthemoon",
+  },
+  {
+    icon: (
+      <img
+        src="https://img.icons8.com/ios-filled/20/ffffff/medium.png"
+        alt="Medium"
+        width={20}
+        height={20}
+      />
+    ),
     href: "https://manofthemooon.medium.com/",
     label: "Medium",
     handle: "manofthemooon",
   },
   {
-    icon: iconUrls.discord,
+    icon: (
+      <img
+        src="https://img.icons8.com/ios-filled/20/ffffff/discord.png"
+        alt="Discord"
+        width={20}
+        height={20}
+      />
+    ),
     href: "https://discordapp.com/users/542318439633780736",
     label: "Discord",
     handle: "manofthemooon",
@@ -70,7 +102,7 @@ export default function Example() {
                   aria-hidden="true"
                 />
                 <span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
-                  <img src={s.icon} alt={`${s.label} icon`} className="w-5 h-5 md:w-6 md:h-6" />
+                  {s.icon}
                 </span>
                 <div className="z-10 flex flex-col items-center">
                   <span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
