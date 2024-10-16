3377a7222fad4f5e2179c78e8438f8a8a4626651
web3ali3n <thirdsaintmob@gmail.com>
2024-10-16 21:28:38 +0300

    v1

--- app/projects/article.tsx
+++ app/projects/article.tsx
@@ -13,6 +13,7 @@ export const Article: React.FC<Props> = ({ project }) => {
 					<span className="text-xs duration-1000 text-zinc-200 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange">
 						{project.date ? (
 							<time dateTime={new Date(project.date).toISOString()}>
+								
 								{new Date(project.date).getFullYear()}
 							</time>
 						) : (
@@ -20,23 +21,30 @@ export const Article: React.FC<Props> = ({ project }) => {
 						)}
 					</span>
 				</div>
-				<h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
-					{project.title}
-				</h2>
+
+				
+				<div className="flex items-center justify-between mt-2">
+					<h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
+						{project.title}
+					</h2>
+					{project.logo && (
+						<img
+							src={project.logo}
+							alt={`${project.title} logo`}
+							className="ml-2 w-8 h-8 -mt-1" 
+						/>
+					)}
+				</div>
+
 				<p className="z-20 mt-4 text-sm duration-1000 text-zinc-400 group-hover:text-zinc-200">
 					{project.description}
 				</p>
+
+				
 				{project.position && (
-					<p className="z-20 mt-10 text-2xl duration-1000 text-zinc-400 group-hover:text-zinc-200 text-center">
+					<p className="z-20 mt-4 text-xs duration-1000 text-zinc-400 group-hover:text-zinc-200 text-center">
 						{project.position}
 					</p>
-				)}
-				{project.logo && (
-					<img
-						src={project.logo}
-						alt={`${project.title} logo`}
-						className="absolute top-1 right-1 w-8 h-8 -mt-0.5"
-					/>
 				)}
 			</article>
 		</Link>
