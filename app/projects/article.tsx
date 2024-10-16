170c9f6f46eda32dd801cc6ea0f7b96dbad3facd
web3ali3n <thirdsaintmob@gmail.com>
2024-10-16 18:19:43 +0300

    v1

--- app/projects/article.tsx
+++ app/projects/article.tsx
@@ -28,7 +28,6 @@ export const Article: React.FC<Props> = ({ project }) => {
 				<p className="z-20 mt-4 text-sm duration-1000 text-zinc-400 group-hover:text-zinc-200">
 					{project.description}
 				</p>
-				
 				{project.logo && (
 					<img
 						src={project.logo}
