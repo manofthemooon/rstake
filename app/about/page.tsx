f2f833e7f42f0381368fecb2513dc3fe46dff32e
web3ali3n <thirdsaintmob@gmail.com>
2024-10-16 05:01:07 +0300

    v1

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 
-import React from 'react';
-import Link from 'next/link';
-import Particles from '../components/particles'; // Путь к вашему компоненту Particles
+import Link from "next/link";
+import Particles from '../components/particles';
+import { Navigation } from "../components/nav";
 
 const About = () => {
   return (
     <div className="relative flex flex-col items-center justify-center w-screen h-screen bg-gradient-to-tl from-black via-zinc-600/20 to-black">
       <Particles className="absolute inset-0 -z-10" quantity={100} />
+      <Navigation />
       <h1 className="text-4xl text-white">About Me</h1>
       <p className="mt-4 text-lg text-zinc-300 text-center">
         Hello! I'm a passionate developer with experience in various technologies. 
