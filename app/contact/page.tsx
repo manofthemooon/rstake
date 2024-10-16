5561d9959e65202f9d371e9d89fada025e913a00
web3ali3n <thirdsaintmob@gmail.com>
2024-10-16 05:52:57 +0300

    v1

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Github, Mail, Twitter, Telegram, Medium, Discord } from "lucide-react";
+import { Github, Mail, Twitter, Medium, Discord } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
