925493c3d4ed9590731320332e6c5e0f1e1e7309
web3ali3n <thirdsaintmob@gmail.com>
2024-10-16 15:53:36 +0300

    v1

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
-import { Card } from "../components/card";
+import { Card } from "../components/card"; 
 
 const socials = [
   {
@@ -94,10 +94,10 @@ export default function Example() {
   return (
     <div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
       <Navigation />
-      <div className="container flex items-center justify-center min-h-screen px-4 mx-auto mt-5">
-        <div className="grid w-full grid-cols-1 gap-2 mx-auto mt-4 mb-4 sm:grid-cols-2 lg:grid-cols-3 lg:gap-6">
+      <div className="container flex items-center justify-center min-h-screen px-4 mx-auto mt-10 mb-10"> {/* Добавлены отступы сверху и снизу */}
+        <div className="grid w-full grid-cols-2 gap-4 mx-auto sm:grid-cols-2 lg:grid-cols-3 lg:gap-8">
           {socials.map((s) => (
-            <Card key={s.label} className="h-64">
+            <Card key={s.label} className="h-56 sm:h-64"> 
               <Link
                 href={s.href}
                 target="_blank"
@@ -122,6 +122,7 @@ export default function Example() {
             </Card>
           ))}
         </div>
+        <div className="mb-20" /> 
       </div>
     </div>
   );
