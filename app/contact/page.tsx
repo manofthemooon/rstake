c6779fb176f16d7566111205a8ffb5d63a0f825b
web3ali3n <thirdsaintmob@gmail.com>
2024-10-16 06:58:54 +0300

    v1

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,82 +1,129 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
-	{
-		icon: <Twitter size={20} />,
-		href: "http://x.com/manoofthemooon",
-		label: "Twitter",
-		handle: "@manoofthemooon",
-	},
-	{
-		icon: <Mail size={20} />,
-		href: "mailto:manoofthemoonwork@gmail.com",
-		label: "Email",
-		handle: "manoofthemoonwork\ngmail.com",
-	},
-	{
-		icon: <Github size={20} />,
-		href: "https://github.com/manofthemooon/",
-		label: "GitHub",
-		handle: "manofthemooon",
-	},
-	{
-		icon: <Telegram size={20} />,
-		href: "https://t.me/manofthemoon",
-		label: "Telegram",
-		handle: "@manofthemooon",
-	},
-	{
-		icon: <Medium size={20} />,
-		href: "https://manofthemooon.medium.com/",
-		label: "Medium",
-		handle: "manofthemooon",
-	},
-	{
-		icon: <Discord size={20} />,
-		href: "https://discordapp.com/users/542318439633780736",
-		label: "Discord",
-		handle: "manofthemooon",
-	},
+  {
+    icon: (
+      <img
+        src="https://img.icons8.com/ios-filled/20/ffffff/twitter--v1.png"
+        alt="Twitter"
+        width={20}
+        height={20}
+      />
+    ),
+    href: "https://x.com/manofthemooon",
+    label: "Twitter",
+    handle: "@manoofthemooon",
+  },
+  {
+    icon: (
+      <img
+        src="https://img.icons8.com/ios-filled/20/ffffff/telegram-app.png"
+        alt="Telegram"
+        width={20}
+        height={20}
+      />
+    ),
+    href: "https://t.me/manofthemoon",
+    label: "Telegram",
+    handle: "@manoofthemooon",
+  },
+  {
+    icon: (
+      <img
+        src="https://img.icons8.com/ios-filled/20/ffffff/github.png"
+        alt="GitHub"
+        width={20}
+        height={20}
+      />
+    ),
+    href: "https://github.com/manofthemooon",
+    label: "GitHub",
+    handle: "manofthemooon",
+  },
+  {
+    icon: (
+      <img
+        src="https://img.icons8.com/ios-filled/20/ffffff/medium.png"
+        alt="Medium"
+        width={20}
+        height={20}
+      />
+    ),
+    href: "https://manofthemooon.medium.com/",
+    label: "Medium",
+    handle: "manofthemooon",
+  },
+  {
+    icon: (
+      <img
+        src="https://img.icons8.com/ios-filled/20/ffffff/discord.png"
+        alt="Discord"
+        width={20}
+        height={20}
+      />
+    ),
+    href: "https://discordapp.com/users/542318439633780736",
+    label: "Discord",
+    handle: "manofthemooon",
+  },
+  {
+    icon: (
+      <img
+        src="https://img.icons8.com/ios-filled/20/ffffff/email.png"
+        alt="Email"
+        width={20}
+        height={20}
+      />
+    ),
+    href: "mailto:manofthemoonwork@gmail.com",
+    label: "Email",
+    handle: (
+      <div className="text-center">
+        manofthemoonwork
+        <br />
+        @gmail.com
+      </div>
+    ),
+  },
 ];
 
 export default function Example() {
-	return (
-		<div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
-			<Navigation />
-			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto mt-20">
-				<div className="grid w-full grid-cols-1 gap-4 mx-auto sm:grid-cols-2 lg:grid-cols-3 lg:gap-8">
-					{socials.map((s) => (
-						<Card key={s.label} className="h-40">
-							<Link
-								href={s.href}
-								target="_blank"
-								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-12 lg:pb-24 md:p-16"
-							>
-								<span
-									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
-									aria-hidden="true"
-								/>
-								<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
-									{s.icon}
-								</span>
-								<div className="z-10 flex flex-col items-center">
-									<span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
-										{s.handle}
-									</span>
-									<span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
-										{s.label}
-									</span>
-								</div>
-							</Link>
-						</Card>
-					))}
-				</div>
-				<div className="mb-20" /> {/* Отступ снизу после карточек */}
-			</div>
-		</div>
-	);
+  return (
+    <div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+      <Navigation />
+      <div className="container flex items-center justify-center min-h-screen px-4 mx-auto mt-10">
+        <div className="grid w-full grid-cols-1 gap-4 mx-auto sm:grid-cols-2 lg:grid-cols-3 lg:gap-8">
+          {socials.map((s) => (
+            <Card key={s.label} className="h-48"> 
+              <Link
+                href={s.href}
+                target="_blank"
+                className="p-4 relative flex flex-col items-center gap-2 duration-700 group md:gap-4 md:py-10 lg:pb-20 md:p-8"
+              >
+                <span
+                  className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
+                  aria-hidden="true"
+                />
+                <span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+                  {s.icon}
+                </span>
+                <div className="z-10 flex flex-col items-center">
+                  <span className="lg:text-lg font-medium duration-150 xl:text-2xl text-zinc-200 group-hover:text-white font-display">
+                    {s.handle}
+                  </span>
+                  <span className="mt-2 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
+                    {s.label}
+                  </span>
+                </div>
+              </Link>
+            </Card>
+          ))}
+        </div>
+        <div className="mb-20" /> {/* Отступ снизу после карточек */}
+      </div>
+    </div>
+  );
 }
