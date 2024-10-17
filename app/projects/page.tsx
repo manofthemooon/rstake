9373feae277787fd0efe5510c03de133b218e8ce
web3ali3n <thirdsaintmob@gmail.com>
2024-10-17 03:09:22 +0300

    v1

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,7 +4,7 @@ import { allProjects } from "contentlayer/generated";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 import { Article } from "./article";
-import Particles from "../components/Particles;
+import Particles from "../components/Particles";
 import { Redis } from "@upstash/redis"; 
 
 
