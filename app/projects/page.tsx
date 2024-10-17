b9d1c66234e608f98031898c67301c0c52de31b6
web3ali3n <thirdsaintmob@gmail.com>
2024-10-17 16:29:20 +0300

    v1

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -18,7 +18,7 @@ export default async function ProjectsPage() {
     return acc;
   }, {} as Record<string, number>);
 
-  const featured = allProjects.find((project) => project.slug === "web3ali3n.dev")!;
+  const featured = allProjects.find((project) => project.slug === "superprotocol")!;
   const top2 = allProjects.find((project) => project.slug === "hyperlane")!;
   const top3 = allProjects.find((project) => project.slug === "injective")!;
   const sorted = allProjects
