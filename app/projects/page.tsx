0ae84be0fc773c84d0b8175ecf8715286613f4a8
web3ali3n <thirdsaintmob@gmail.com>
2024-10-17 03:01:15 +0300

    v1

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,6 +4,7 @@ import { allProjects } from "contentlayer/generated";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 import { Article } from "./article";
+import Particles from "./components/particles";
 import { Redis } from "@upstash/redis";
 
 const redis = Redis.fromEnv();
@@ -37,6 +38,11 @@ export default async function ProjectsPage() {
   return (
     <div className="relative pb-16">
       <Navigation />
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <Particles
+        className="absolute inset-0 -z-10 animate-fade-in"
+        quantity={100}
+      />
       <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
         <div className="max-w-2xl mx-auto lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
