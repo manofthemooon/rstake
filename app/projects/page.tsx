f74e0561cc3036dd00a8e43d9e6d9b26288d6721
web3ali3n <thirdsaintmob@gmail.com>
2024-10-17 16:57:08 +0300

    v1

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -59,7 +59,7 @@ export default async function ProjectsPage() {
                   <div className="flex items-center justify-between gap-2">
                     <div className="text-xs text-zinc-100">
                       {featured.date ? (
-                        <time dateTime={new Date(featured.date).toISOString()} className="block mt-64">
+                        <time dateTime={new Date(featured.date).toISOString()} className="block mt-24">
                           {new Date(featured.date).getFullYear()}
                         </time>
                       ) : (
