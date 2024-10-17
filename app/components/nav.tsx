4be1ac3e1a1c63a0d69b87f763c823301bcc21b4
web3ali3n <thirdsaintmob@gmail.com>
2024-10-17 18:49:30 +0300

    v1

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -35,11 +35,13 @@ export const Navigation: React.FC = () => {
 					</Link>
 
 					<div className="absolute left-1/2 transform -translate-x-1/2 z-0">
-						<img
-							src="https://i.ibb.co/f0kTqbx/web3ali3n.png"
-							alt="Web3 Ali3n Logo"
-							className="h-10 w-10 object-contain md:h-16 md:w-16 lg:h-24 lg:w-24 my-2 sm:my-4"
-						/>
+						<Link href="https://web3ali3n.vercel.app/" target="_blank" rel="noopener noreferrer">
+							<img
+								src="https://i.ibb.co/f0kTqbx/web3ali3n.png"
+								alt="Web3 Ali3n Logo"
+								className="h-10 w-10 object-contain md:h-16 md:w-16 lg:h-24 lg:w-24 my-2 sm:my-4"
+							/>
+						</Link>
 					</div>
 
 					<div className="flex gap-8 z-10">
