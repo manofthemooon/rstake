7d5c63d8ddaca778c9fd0e3031be027a1ceb2a22
web3ali3n <thirdsaintmob@gmail.com>
2024-10-17 17:08:19 +0300

    v1

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -26,23 +26,23 @@ export const Navigation: React.FC = () => {
 						: "bg-zinc-900/500 border-zinc-800"
 				}`}
 			>
-				<div className="container flex items-center justify-between p-6 mx-auto">
+				<div className="container relative flex items-center justify-between p-6 mx-auto">
 					<Link
 						href="/"
-						className="duration-200 text-zinc-300 hover:text-zinc-100"
+						className="z-10 duration-200 text-zinc-300 hover:text-zinc-100"
 					>
 						<ArrowLeft className="w-6 h-6" />
 					</Link>
 
-					<div className="absolute inset-0 flex justify-center items-center">
+					<div className="absolute left-1/2 transform -translate-x-1/2 z-0">
 						<img
 							src="https://i.ibb.co/f0kTqbx/web3ali3n.png"
 							alt="Web3 Ali3n Logo"
-							className="h-16 w-16 object-contain md:h-24 md:w-24 lg:h-32 lg:w-32 my-2 sm:my-4"
+							className="h-10 w-10 object-contain md:h-16 md:w-16 lg:h-24 lg:w-24 my-2 sm:my-4"
 						/>
 					</div>
 
-					<div className="flex gap-8">
+					<div className="flex gap-8 z-10">
 						<Link
 							href="/about"
 							className="duration-200 text-zinc-400 hover:text-zinc-100"
