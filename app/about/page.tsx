823e19153bb01c8549528ef8384009789632b9a5
web3ali3n <thirdsaintmob@gmail.com>
2024-10-21 06:16:42 +0300

    v1

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,9 +12,9 @@ const AboutPage: React.FC = () => {
     <div className="relative flex h-screen w-screen bg-black">
       <Navigation />
       <Particles className="absolute inset-0 -z-10" quantity={100} />
-      <div className="flex flex-row justify-between items-center w-full h-full p-8">
-        <div className="text-left text-white pl-24">
-          <p className="text-sm text-gray-400 typing-effect">{'<Hello, world>'}</p>
+      <div className="flex flex-col md:flex-row justify-between items-center w-full h-full p-8">
+        <div className="text-container text-left text-white pl-24 md:pl-8">
+          <p className="text-sm text-gray-400 typing-effect">{'Hello, world'}</p>
           <h1 className="text-4xl md:text-6xl font-display mt-4">
             My name is Andrey.
           </h1>
@@ -22,7 +22,7 @@ const AboutPage: React.FC = () => {
             I am a Web3 enjoyer from Russia.
           </h2>
         </div>
-        <div className="w-1/2 h-full">
+        <div className="canvas-container w-full md:w-1/2 h-full">
           <Canvas className="w-full h-full">
             <RotatingPoints />
           </Canvas>
