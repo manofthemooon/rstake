eef1596c6c1e410ce94dbb98b681575029d5ed53
web3ali3n <thirdsaintmob@gmail.com>
2024-10-22 03:21:36 +0300

    v1

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -33,23 +33,47 @@ const AboutPage: React.FC = () => {
 };
 
 const RotatingPoints = () => {
-  const pointsRef = useRef<THREE.Points>(null);
-
-  useFrame(() => {
-    if (pointsRef.current) {
-      pointsRef.current.rotation.y += 0.01;
-    }
-  });
-
-  const pointsGeometry = new THREE.SphereGeometry(1.5, 32, 32);
-  const pointsMaterial = new THREE.PointsMaterial({
-    color: 'white',
-    size: 0.01,
-  });
-
-  return (
-    <primitive object={new THREE.Points(pointsGeometry, pointsMaterial)} ref={pointsRef} />
-  );
-};
-
-export default AboutPage;
+	const pointsRef = useRef<THREE.Points>(null);
+  
+	useFrame(() => {
+	  if (pointsRef.current) {
+		pointsRef.current.rotation.y += 0.002;
+		pointsRef.current.rotation.x += 0.001;
+	  }
+	});
+  
+	const particlesCount = 1000;
+	const positions = new Float32Array(particlesCount * 3);
+	for (let i = 0; i < particlesCount; i++) {
+	  const r = 1.5; // Радиус сферы
+	  const theta = 2 * Math.PI * Math.random(); // Угол
+	  const phi = Math.acos(2 * Math.random() - 1); // Высота
+  
+	  const x = r * Math.sin(phi) * Math.cos(theta);
+	  const y = r * Math.sin(phi) * Math.sin(theta);
+	  const z = r * Math.cos(phi);
+  
+	  positions[i * 3] = x;
+	  positions[i * 3 + 1] = y;
+	  positions[i * 3 + 2] = z;
+	}
+  
+	const pointsGeometry = new THREE.BufferGeometry();
+	pointsGeometry.setAttribute(
+	  'position',
+	  new THREE.BufferAttribute(positions, 3)
+	);
+  
+	const pointsMaterial = new THREE.PointsMaterial({
+	  color: 'white',
+	  size: 0.02, 
+	  opacity: 0.8, 
+	  transparent: true,
+	});
+  
+	return (
+	  <points ref={pointsRef} geometry={pointsGeometry} material={pointsMaterial} />
+	);
+  };
+  
+  export default AboutPage;
