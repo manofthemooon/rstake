de8576841531344e521b9076be9fce7cde235457
web3ali3n <thirdsaintmob@gmail.com>
2024-10-22 03:36:02 +0300

    v1

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -32,45 +32,50 @@ const AboutPage: React.FC = () => {
   );
 };
 
-const AbstractRotatingShape = () => {
-  const pointsRef = useRef<THREE.Points>(null);
+const RotatingPoints = () => {
+	const pointsRef = useRef<THREE.Points>(null);
+  
+	useFrame(() => {
+	  if (pointsRef.current) {
+		pointsRef.current.rotation.y += 0.002;
+		pointsRef.current.rotation.x += 0.001;
+	  }
+	});
+  
 
-  useFrame(() => {
-    if (pointsRef.current) {
-      pointsRef.current.rotation.y += 0.002;
-      pointsRef.current.rotation.x += 0.001;
-    }
-  });
-
-
-  const particleCount = 20000; 
-  const positions = new Float32Array(particleCount * 3);
-
-  for (let i = 0; i < particleCount; i++) {
-    const radius = 2.5;
-    const theta = Math.random() * 2 * Math.PI;
-    const phi = Math.random() * Math.PI;
-
-    const x = radius * Math.sin(phi) * Math.cos(theta);
-    const y = radius * Math.sin(phi) * Math.sin(theta);
-    const z = radius * Math.cos(phi);
-
-    positions[i * 3] = x;
-    positions[i * 3 + 1] = y;
-    positions[i * 3 + 2] = z;
-  }
-
-  const geometry = new THREE.BufferGeometry();
-  geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-
-  const material = new THREE.PointsMaterial({
-    color: 0xffffff,
-    size: 0.015, 
-    transparent: true,
-    opacity: 0.8, 
-  });
-
-  return <points ref={pointsRef} geometry={geometry} material={material} />;
-};
-
-export default AboutPage;
+	const particlesCount = 10000; 
+	const positions = new Float32Array(particlesCount * 3);
+	
+	for (let i = 0; i < particlesCount; i++) {
+	  const r = 2.5; 
+	  const theta = 2 * Math.PI * Math.random(); 
+	  const phi = Math.PI * Math.random(); 
+  
+	  const x = r * Math.sin(phi) * Math.cos(theta);
+	  const y = r * Math.sin(phi) * Math.sin(theta);
+	  const z = r * Math.cos(phi) * Math.sin(theta); 
+  
+	  positions[i * 3] = x;
+	  positions[i * 3 + 1] = y;
+	  positions[i * 3 + 2] = z;
+	}
+  
+	const pointsGeometry = new THREE.BufferGeometry();
+	pointsGeometry.setAttribute(
+	  'position',
+	  new THREE.BufferAttribute(positions, 3)
+	);
+  
+	const pointsMaterial = new THREE.PointsMaterial({
+	  color: 'white',
+	  size: 0.02, 
+	  opacity: 0.7, 
+	  transparent: true,
+	});
+  
+	return (
+	  <points ref={pointsRef} geometry={pointsGeometry} material={pointsMaterial} />
+	);
+  };
+  
+  export default AboutPage;
