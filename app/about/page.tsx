26cfebffa9ad9b968705afb74e7086c56f1c8d10
web3ali3n <thirdsaintmob@gmail.com>
2024-10-22 17:38:09 +0300

    v1

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -47,9 +47,9 @@ const AboutPage: React.FC = () => {
       <ScrollIndicators activeBlock={activeBlock} onClick={handleClick} blockCount={blockCount} />
 
       <div className="about-snap-block flex items-center justify-center h-screen">
-        <div className="text-container text-left text-white">
-          <h1 className="text-4xl md:text-6xl font-display mt-4">My name is Andrey.</h1>
-          <h2 className="text-2xl md:text-4xl font-sans mt-2">I am a Web3 enjoyer from Russia.</h2>
+        <div className="text-container text-left text-white glow-effect">
+          <h1 className="text-4xl md:text-6xl font-display mt-4 glow-text">My name is Andrey.</h1>
+          <h2 className="text-2xl md:text-4xl font-sans mt-2 glow-text">I am a Web3 enjoyer from Russia.</h2>
         </div>
         <div className="canvas-container w-full md:w-[37.5%] h-full">
           <Canvas className="w-full h-full">
@@ -59,29 +59,29 @@ const AboutPage: React.FC = () => {
       </div>
 
       <div className="about-snap-block flex flex-col justify-between h-screen p-8">
-        <div className="education-info text-left text-white">
+        <div className="education-info text-left text-white cosmic-border p-4">
           <h3 className="text-2xl font-bold mt-12">Education</h3>
           <ul className="mt-4 space-y-2">
             <li>Bachelor’s Degree in Infocommunication Technologies, MTUCI (2019-2023)</li>
             <li>Master’s Degree in Quantum Communications, MTUCI (2023-2025)</li>
           </ul>
         </div>
-        <div className="tech-stack text-right text-white">
+        <div className="tech-stack text-right text-white cosmic-border p-4">
           <h3 className="text-2xl font-bold">Tech Stack</h3>
           <div className="flex flex-wrap justify-end gap-2 mt-4">
             {['React', 'TypeScript', 'Python', 'C#', 'HTML/CSS', 'DevTools', 'XPath', 'Git', 'Jira', 'ZennoPoster', 'YouGile', 'API'].map((tool) => (
-              <span key={tool} className="bg-gray-800 text-white px-3 py-1 rounded-md">{tool}</span>
+              <span key={tool} className="bg-gray-800 text-white px-3 py-1 rounded-md glow-border">{tool}</span>
             ))}
           </div>
         </div>
       </div>
 
       <div className="about-snap-block flex items-center justify-center h-screen">
-        <h1 className="text-4xl text-white">Third Block Content</h1>
+        <h1 className="text-4xl text-white glow-text">Third Block Content</h1>
       </div>
 
       <div className="about-snap-block flex items-center justify-center h-screen">
-        <h1 className="text-4xl text-white">Fourth Block Content</h1>
+        <h1 className="text-4xl text-white glow-text">Fourth Block Content</h1>
       </div>
     </div>
   );
