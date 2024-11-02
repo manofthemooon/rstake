f4a01bb95515eb98b9775fa3c2ce611b632695ff
web3ali3n <thirdsaintmob@gmail.com>
2024-11-02 04:48:20 +0300

    v1

--- app/components/imageWorker.ts
+++ app/components/imageWorker.ts
@@ -1,35 +1,12 @@
 self.onmessage = (e) => {
-  const { data, width, height, scale } = e.data;
-  const points: number[] = [];
-  const threshold = 128;
+  const { data, width, height, density, scale } = e.data;
+  const points = [];
 
-  for (let y = 0; y < height; y += 2) {
-    let linePoints: number[] = [];
-    let isDrawing = false;
-
-    for (let x = 0; x < width; x++) {
-      const i = (y * width + x) * 4;
-      const brightness = (data[i] + data[i + 1] + data[i + 2]) / 3;
-
-      if (brightness < threshold) {
-        linePoints.push(
-          (x - width / 2) * scale / width,
-          -(y - height / 2) * scale / height,
-          0
-        );
-        isDrawing = true;
-      } else if (isDrawing) {
-        if (linePoints.length > 0) {
-          points.push(...linePoints);
-          points.push(...linePoints.slice(-3));
-        }
-        linePoints = [];
-        isDrawing = false;
-      }
-    }
-
-    if (linePoints.length > 0) {
-      points.push(...linePoints);
+  for (let i = 0; i < data.length; i += 4) {
+    if (data[i + 3] > 0 && Math.random() < density) {
+      const x = ((i / 4) % width - width / 2) * scale;
+      const y = (Math.floor((i / 4) / width) - height / 2) * scale;
+      points.push(x, -y, 0);
     }
   }
 
