eaae5b3ada146a6a74202dfe089c3c307161454b
web3ali3n <thirdsaintmob@gmail.com>
2024-11-05 19:46:55 +0300

    v1

--- content/articles-data.ts
+++ content/articles-data.ts
@@ -1,27 +1,27 @@
 export const articles = [
   { 
     title: "Ocean Protocol: Empowering a Data-Driven Future", 
-    image: "/articles/article1.png", 
+    image: "../../content/articles/article1.png", 
     link: "https://manofthemooon.medium.com/ocean-protocol-empowering-a-data-driven-future-d590bab9d55d" 
   },
   { 
     title: "Injective Protocol: Revolutionizing Decentralized Exchange", 
-    image: "/articles/article2.png", 
+    image: "../../content/articles/article2.png", 
     link: "https://manofthemooon.medium.com/injective-protocol-revolutionizing-decentralized-exchange-c5bfa8ef54a" 
   },
   { 
     title: "Introducing Uroboros Wallet", 
-    image: "/articles/article3.png", 
+    image: "../../content/articles/article3.png", 
     link: "https://manofthemooon.medium.com/introducing-uroboros-wallet-the-solution-for-seamless-defi-experience-b3d3ead45281" 
   },
   { 
     title: "Analyzing Injective (INJ) Crypto", 
-    image: "/articles/article4.png", 
+    image: "../../content/articles/article4.png", 
     link: "https://manofthemooon.medium.com/analyzing-injective-inj-crypto-price-consolidation-and-future-potential-b3743854b71f" 
   },
   { 
     title: "Hyperlane ISM", 
-    image: "/articles/article5.png", 
+    image: "../../content/articles/article5.png", 
     link: "https://manofthemooon.medium.com/ru-hyperlane-isms-%D1%87%D1%82%D0%BE-%D1%8D%D1%82%D0%BE-748047675b89" 
   },
 ];
@@ -29,27 +29,27 @@ export const articles = [
 export const threads = [
   { 
     title: "Ocean Protocol Toolset: Ocean.py", 
-    image: "/threads/thread1.png", 
+    image: "../../content/threads/thread1.png", 
     link: "https://x.com/manoofthemooon/status/1671136940140142592" 
   },
   { 
     title: "Venom: A Blockchain Without Boundaries", 
-    image: "/threads/thread2.png", 
+    image: "../../content/threads/thread2.png", 
     link: "https://x.com/manoofthemooon/status/1664322123278565389" 
   },
   { 
     title: "Ocean Protocol InvestorsObserver Analysis", 
-    image: "/threads/thread3.png", 
+    image: "../../content/threads/thread3.png", 
     link: "https://x.com/manoofthemooon/status/1679111462789234688" 
   },
   { 
     title: "Injective SWOT Analysis", 
-    image: "/threads/thread4.png", 
+    image: "../../content/threads/thread4.png", 
     link: "https://x.com/manoofthemooon/status/1661490930304663553" 
   },
   { 
     title: "Decentralized Music Data Marketplace with Ocean Protocol", 
-    image: "/threads/thread5.png", 
+    image: "../../content/threads/thread5.png", 
     link: "https://x.com/manoofthemooon/status/1676230380158844928" 
   },
 ]; 
