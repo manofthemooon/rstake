f569c3d951718eb53a31c96889d20e1eeb9d9503
web3ali3n <thirdsaintmob@gmail.com>
2024-11-06 06:00:36 +0300

    v1

--- app/components/model.tsx
+++ app/components/model.tsx
@@ -7,20 +7,27 @@ const Logo: React.FC = () => {
   const meshRef = useRef<any>(null);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (meshRef.current) {
-        meshRef.current.rotation.y += 0.01;  
-      }
-    }, 16);
+    let animationFrameId: number;
 
-    return () => clearInterval(interval);
+    const animate = () => {
+      if (meshRef.current) {
+        meshRef.current.rotation.y += 0.01;
+      }
+      animationFrameId = requestAnimationFrame(animate);
+    };
+
+    animate();
+
+    return () => {
+      cancelAnimationFrame(animationFrameId);
+    };
   }, []);
 
   return (
     <mesh ref={meshRef} position={[0, 0, 0]} scale={[0.5, 0.5, 0.5]}>
       <planeGeometry args={[5, 5]} />
       <meshBasicMaterial map={new THREE.TextureLoader().load('/alien.png')} transparent={true} />
-      <meshBasicMaterial map={new THREE.TextureLoader().load('/alien.png')} transparent={true} side={THREE.BackSide} />
+      <meshBasicMaterial map={new THREE.TextureLoader().load('/alien.png')} transparent={true} side={THREE.DoubleSide} />
     </mesh>
   );
 };
