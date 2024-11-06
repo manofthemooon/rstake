b999b0ca91fdad2799cd753ffd7de7ed00f71495
web3ali3n <thirdsaintmob@gmail.com>
2024-11-06 17:36:29 +0300

    v1

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -52,17 +52,17 @@ const AboutPage: React.FC = () => {
       <Navigation />
       <ScrollIndicators activeBlock={activeBlock} onClick={handleClick} blockCount={blockCount} />
 
-      <div className="about-snap-block flex items-center justify-center h-screen relative">
+      <div className="about-snap-block flex flex-col items-center justify-center h-screen relative">
         <Particles className="absolute inset-0 w-full h-full -z-10" quantity={100} />
-        <div className="text-container text-left text-white">
+        <div className="text-container text-center text-white">
           <h1 className="text-4xl md:text-6xl font-display mt-4">My name is Andrey.</h1>
-          <h2 className="text-2xl md:text-4xl font-sans mt-2">I am a Web3 enjoyer from Russia.</h2>
+          <h2 className="text-2xl md:text-4xl font-sans mt-2">I'm Making Crypto Projects Better</h2>
         </div>
-        <div className="canvas-container w-full md:w-[37.5%] h-full">
-        <Canvas camera={{ position: [0, 1, 5], fov: 50 }}>
+        <div className="canvas-container-logo w-full md:w-[37.5%] h-full">
+          <Canvas camera={{ position: [0, 1, 5], fov: 50 }}>
             <ambientLight intensity={0.5} />
             <pointLight position={[10, 10, 10]} intensity={1} />
-            <Logo /> 
+            <Logo />
           </Canvas>
         </div>
       </div>
