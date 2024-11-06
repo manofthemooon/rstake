e3f998d478291a5b473353ea51105f15958d5d26
web3ali3n <thirdsaintmob@gmail.com>
2024-11-06 04:28:22 +0300

    v1

--- app/components/model.tsx
+++ app/components/model.tsx
@@ -1,5 +1,5 @@
 import { useRef, useState, useEffect } from 'react';
-import { useFrame } from '@react-three/fiber';
+import { useFrame, Canvas } from '@react-three/fiber';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
@@ -9,7 +9,7 @@ const Model: React.FC = () => {
 
   useEffect(() => {
     const loader = new GLTFLoader();
-    loader.load('/app/components/alien.glb', (gltf) => {
+    loader.load('./alien.glb', (gltf) => {
       const model = gltf.scene;
       model.traverse((child) => {
         if ((child as THREE.Mesh).isMesh) {
@@ -37,7 +37,13 @@ const Model: React.FC = () => {
 
   if (!modelLoaded) return null;
 
-  return <primitive ref={modelRef} object={modelRef.current || new THREE.Group()} />;
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} intensity={1} />
+      <primitive ref={modelRef} object={modelRef.current || new THREE.Group()} />
+    </Canvas>
+  );
 };
 
 export default Model;
