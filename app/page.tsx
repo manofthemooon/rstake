10faeea89145981e7ec3b66be6f7b89b0b172e0c
web3ali3n <thirdsaintmob@gmail.com>
2024-11-24 18:47:10 +0300

    v1

--- app/page.tsx
+++ app/page.tsx
@@ -15,12 +15,12 @@ const Home: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-between w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
       <nav className="my-8 animate-fade-in">
-        <ul className="flex items-center justify-center gap-24">
+        <ul className="flex items-center justify-center gap-30">
           {navigation.map((item) => (
             <Link
               key={item.href}
               href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300 font-gtwalsheimprolight"
+              className="text-sm duration-500 text-white hover:text-zinc-300 hover:underline font-gtwalsheimpro"
             >
               {item.name}
             </Link>
